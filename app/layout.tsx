0f1a84960207307675fa726c56a574174eb9c208
rahulsaini-js <itsrahulsaini21@gmail.com>
2025-03-22 08:47:47 +0530

    setting up folder sturucture and assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Mona_Sans({
+const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
 });
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${monaSans.className} antialiased`}
       >
         {children}
       </body>
